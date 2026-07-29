6d1220d587b72874803b532fc0c38adf0db5fba7
Gonfer73 <ferrergonzalo988@gmail.com>
2026-07-29 11:25:47 +0200

    Añadidos detalles para que cada caso sea único, entre ellos la aleatoridad de los nombres

--- corruption/case_01/scripts/case_01.gd
+++ corruption/case_01/scripts/case_01.gd
@@ -3,9 +3,36 @@ extends Node2D
 var nombres_f = ["Abril", "Aitana", "Alba", "Alicia", "Alma", "Amalia", "Ana", "Andrea", "Ángela", "Ariadna", "Aurora", "Bárbara", "Beatriz", "Belén", "Berta", "Blanca", "Bruna", "Camila", "Candela", "Carla", "Carlota", "Carmen", "Carolina", "Catalina", "Celia", "Clara", "Claudia", "Cristina", "Daniela", "Diana", "Dolores", "Elena", "Elisa", "Elsa", "Emma", "Esther", "Eva", "Fabiola", "Fátima", "Fernanda", "Gabriela", "Gema", "Gloria", "Guadalupe", "Inés", "Irene", "Iris", "Isabel", "Jana", "Jimena", "Julia", "Lara", "Laura", "Leire", "Leonor", "Leticia", "Lidia", "Lola", "Lorena", "Lucía", "Luna", "Macarena", "Manuela", "Mar", "María", "Marina", "Marta", "Martina", "Mía", "Miriam", "Mónica", "Nadia", "Natalia", "Nerea", "Noelia", "Nuria", "Olivia", "Olga", "Paola", "Patricia", "Paula", "Pilar", "Raquel", "Rebecca", "Rocío", "Rosa", "Ruth", "Sabina", "Sara", "Sofía", "Sonia", "Tania", "Teresa", "Valentina", "Valeria", "Vanesa", "Verónica", "Victoria", "Yolanda", "Zoe"]
 var nombres_m = ["Aarón", "Abel", "Adrián", "Agustín", "Alejandro", "Andrés", "Antonio", "Arturo", "Baltasar", "Benjamín", "Bernardo", "Borja", "Bruno", "Camilo", "Carlos", "César", "Claudio", "Cristian", "Damián", "Daniel", "Darío", "David", "Diego", "Edgar", "Eduardo", "Elías", "Emilio", "Enrique", "Esteban", "Fabián", "Federico", "Felipe", "Fernando", "Francisco", "Gabriel", "Gael", "Gerardo", "Gonzalo", "Guillermo", "Héctor", "Hernán", "Hugo", "Ian", "Iker", "Isaac", "Ismael", "Iván", "Jaime", "Javier", "Jesús", "Joaquín", "Jorge", "José", "Juan", "Kevin", "Kilian", "Leandro", "Leo", "Leonardo", "Lorenzo", "Lucas", "Luis", "Manuel", "Marcos", "Mario", "Martín", "Mateo", "Matías", "Miguel", "Nicolás", "Noé", "Oliver", "Omar", "Óscar", "Pablo", "Patricio", "Pedro", "Rafael", "Ramiro", "Ramón", "Raúl", "Ricardo", "Roberto", "Rodrigo", "Rubén", "Salvador", "Samuel", "Santiago", "Sebastián", "Sergio", "Tadeo", "Tomás", "Ulises", "Uriel", "Vicente", "Víctor", "Walter", "Xavier", "Yago", "Zacarías"]
 var apellidos = ["Abad", "Aguilar", "Alonso", "Álvarez", "Arias", "Barrios", "Beltrán", "Benítez", "Blanco", "Bravo", "Burgos", "Cabrera", "Calvo", "Campos", "Cano", "Carrasco", "Castillo", "Castro", "Cortés", "Cruz", "Delgado", "Díaz", "Domínguez", "Espinosa", "Estévez", "Fernández", "Ferrer", "Flores", "Fuentes", "Gallardo", "Gallego", "García", "Giménez", "Gómez", "González", "Guerrero", "Gutiérrez", "Hernández", "Herrera", "Hidalgo", "Ibáñez", "Iglesias", "Jiménez", "Juárez", "Lara", "León", "López", "Lozano", "Luna", "Marín", "Márquez", "Martín", "Martínez", "Medina", "Méndez", "Mendoza", "Miranda", "Molina", "Montero", "Morales", "Moreno", "Muñoz", "Navarro", "Nieto", "Núñez", "Ortega", "Ortiz", "Pacheco", "Padilla", "Palacios", "Pardo", "Parra", "Pascual", "Pastor", "Pérez", "Prieto", "Quintana", "Quiroga", "Ramírez", "Ramos", "Rivera", "Robles", "Rodríguez", "Rojas", "Romero", "Rubio", "Ruiz", "Saavedra", "Sáez", "Salazar", "Sanz", "Serrano", "Silva", "Soto", "Suárez", "Torres", "Vargas", "Vázquez", "Vega", "Zúñiga"]
+var masculino_0 = 0
+var masculino_1 = 0
+var nombre_0 = "a"
+var nombre_1 = "a"
 #nombres y apellidos disponibles para los personajes
 
-
-var nombre = nombres_f.pick_random() + " " + apellidos.pick_random() + " " + apellidos.pick_random()
-func _ready() -> void:
-	print(nombre)
+func _ready():
+	randomize()
+	masculino_0 = randi_range(0, 1)
+	masculino_1 =  randi_range(0, 1)
+	$Paper01/label_06.text = str(randi_range(18, 60)) + " " + "Años"
+	$Paper01/label_07.text = str(randi_range(18, 60)) + " " + "Años"
+	if masculino_0 == 0:
+		$Paper01/label_08.text = "Mujer"
+		nombre_0 = nombres_f.pick_random() + " " + apellidos.pick_random() + " " + apellidos.pick_random()
+		print(nombre_0)
+		$Paper01/label_03.text = nombre_0
+	elif masculino_0 == 1:
+		$Paper01/label_08.text = "Hombre"
+		nombre_0 = nombres_m.pick_random() + " " + apellidos.pick_random() + " " + apellidos.pick_random()
+		print(nombre_0)
+		$Paper01/label_03.text = nombre_0
+	if masculino_1 == 0:
+		$Paper01/label_09.text = "Mujer"
+		nombre_1 = nombres_f.pick_random() + " " + apellidos.pick_random() + " " + apellidos.pick_random()
+		print(nombre_1)
+		$Paper01/label_05.text = nombre_1
+	elif masculino_1 == 1:
+		$Paper01/label_09.text = "Hombre"
+		nombre_1 = nombres_m.pick_random() + " " + apellidos.pick_random() + " " + apellidos.pick_random()
+		print(nombre_1)
+		$Paper01/label_05.text = nombre_1
+	
